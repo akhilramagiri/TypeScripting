61ecf4ed18d45f243edc570063c9e8c44b30243b
akhilramagiri <akhil.ramagiri@iboa.com.au>
2024-10-16 14:27:08 +1100

    first commit

---
+++ fetchjson/index.ts
@@ -0,0 +1,9 @@
+// Make a network request to fetch some JSON and print the result
+
+import axios from "axios";
+
+const url = "https://jsonplaceholder.typicode.com/todos/1";
+
+axios.get(url).then( response => {
+  console.log(response.data);
+})
